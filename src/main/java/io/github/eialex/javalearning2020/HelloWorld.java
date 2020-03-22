fd18d3a194090d6ea619412228a6943b305d6348
Álex Junio <alexshox@gmail.com>
2020-03-22 14:56:09 -0300

    Hello World!

---
+++ src/main/java/io/github/eialex/javalearning2020/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.eialex.javalearning2020;
+
+/**
+ *
+ * @author lexsh
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" O Famoso Hello World, para manter a tradição!");
+    }
+    
+}
